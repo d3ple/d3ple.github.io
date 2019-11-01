9798bc227fa5a6c84e871e67ff4133d7d4ead70a
Danil <d.danil.d2@gmail.com>
2019-11-02 04:49:08 +0700

    Add kemevents

---
+++ kemevents/precache-manifest.ca0b7f2410e5e7a304ade8bec6497dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b147e43da49982338deeff427ee3640",
+    "url": "/kemevents/index.html"
+  },
+  {
+    "revision": "92d1dfd8dbdc1c7d859a",
+    "url": "/kemevents/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "184209c52f02b8306bd3",
+    "url": "/kemevents/static/css/main.d235777f.chunk.css"
+  },
+  {
+    "revision": "92d1dfd8dbdc1c7d859a",
+    "url": "/kemevents/static/js/2.9aeb17f3.chunk.js"
+  },
+  {
+    "revision": "184209c52f02b8306bd3",
+    "url": "/kemevents/static/js/main.de27458d.chunk.js"
+  },
+  {
+    "revision": "cabe0e366be2e6b287f8",
+    "url": "/kemevents/static/js/runtime-main.91731410.js"
+  }
+]);
