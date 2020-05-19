7c8805765feadabe651ae62afc7e70d83d9d3d9e
Danil Dunaitsev <d.danil.d2@gmail.com>
2020-05-19 19:30:25 +0700

    Добавил лк бтв

---
+++ btv/precache-manifest.7bc5ede25187578027a6c9c2522333f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0de7781b54f7be19995f44d433e0699",
+    "url": "./index.html"
+  },
+  {
+    "revision": "191b52b2fdf8d86e89c2",
+    "url": "./static/css/main.05879178.chunk.css"
+  },
+  {
+    "revision": "6183122498048fc9d8bf",
+    "url": "./static/js/2.b0e42f3d.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "./static/js/2.b0e42f3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "191b52b2fdf8d86e89c2",
+    "url": "./static/js/main.53f2a2ba.chunk.js"
+  },
+  {
+    "revision": "93df77235358fdd5d175",
+    "url": "./static/js/runtime-main.fc4f936c.js"
+  }
+]);
